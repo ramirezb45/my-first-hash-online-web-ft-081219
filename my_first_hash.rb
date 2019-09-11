9899f71b851a35316e9ce8a45bdbfdf71a9cc9fb
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-09-11 01:23:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,6 +30,5 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
- 
+ shipping_manifest
 end 
-puts shipping_manifest
